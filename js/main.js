da67b9f55414a4a45d3909fea92bbab224e060d5
tiohs <hamiltonsilva551@gmail.com>
2021-10-27 08:14:29 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const navHeight = header.offsetHeight;
 const menu = document.querySelector('#menu');
 const infomenu = document.querySelector('#infomenu');
 
+
+
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll');
@@ -11,10 +13,28 @@ function changeHeaderWhenScroll() {
   }
 }
 
-menu.addEventListener('click', () => {
-  menu.classList.add('ative');
-  infomenu.classList.add('ative');
+menu.addEventListener('click', (e) => {
+  if(menu.classList.contains('ative')) {
+    menu.classList.remove('ative');
+    infomenu.classList.remove('ative');
+  } else {
+    menu.classList.add('ative');
+    infomenu.classList.add('ative');
+  }
+  
 });
+
+
+  infomenu.addEventListener('click', (e) => {
+    console.log(e)
+    if(menu.classList.contains('ative') && e.target.classList.contains('item')) {
+      menu.classList.remove('ative');
+      infomenu.classList.remove('ative');
+    }
+  });
+
+
+
 
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll();
